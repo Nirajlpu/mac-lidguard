f5fba4041b340494834e2899af5f29415aef0c8e
nirajlpu <nirajsahani2004@gmail.com>
2026-09-13 00:27:28 +0530

    Initial Commit

---
+++ LidGuard/LidGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidGuardApp.swift
+//  LidGuard
+//
+//  Created by Niraj Kumar on 13/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LidGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
